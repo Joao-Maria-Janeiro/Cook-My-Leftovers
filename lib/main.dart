1a43d02badce8c788e359b91550a0776451f0a56
Guilherme <guilherme.viegas@tecnico.ulisboa.pt>
2019-01-13 00:54:27 +0000

    Widget ListView

--- lib/main.dart
+++ lib/main.dart
@@ -27,11 +27,13 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  
+  int _count = 1;
+
   String result = "";
 
   @override
   Widget build(BuildContext context) {
+    List<Widget> _ingredients = new List.generate(_count, (int i) => new IngredientColumn());
 
     return Scaffold(
       appBar: AppBar(
@@ -48,16 +50,65 @@ class _MyHomePageState extends State<MyHomePage> {
                 ),
                 onSubmitted: (String str){
                   setState(() {
-                    result = str;
-                    Navigator.of(context).push(new MaterialPageRoute(builder: (BuildContext context) => new RecipesStage(ingredients: "flour,sugar,apple")));
+                    result = result + str;
+                    Navigator.of(context).push(new MaterialPageRoute(builder: (BuildContext context) => new RecipesStage(ingredients: result)));
                   });
                 },
               ),
-              new Text(result)
+              new Container(
+                height: 200.0,
+                child: new ListView(
+                  children: _ingredients,
+                  scrollDirection: Axis.vertical,
+                ),
+              ),
+              new FlatButton(
+                onPressed: _addNewIngredientColumn,
+                child: new Icon(Icons.add),
+              ),
+              RaisedButton(
+                color: Colors.amberAccent,
+                shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(4.0)),
+                elevation: 6.0,
+                onPressed: () => Navigator.of(context).push(new MaterialPageRoute(builder: (BuildContext context) => new RecipesStage(ingredients: result))),
+                child: new Text("Check For Recipes"),
+              ),
             ],
           ),
         ),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  void _addNewIngredientColumn() {
+    setState(() {
+      _count = _count + 1;
+    });
+  }
+}
+
+class IngredientColumn extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => new _IngredientColumn();
+}
+
+class _IngredientColumn extends State<IngredientColumn> {
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+        width: 170.0,
+        padding: new EdgeInsets.all(5.0),
+        child: new Column(children: <Widget>[
+          new TextField(
+            decoration: new InputDecoration(
+              labelText: 'Ingredient',
+            ),
+            onSubmitted: (String str){
+            setState(() {
+              //result = result + str;
+            });
+          },
+          ),
+        ]));
   }
 }
